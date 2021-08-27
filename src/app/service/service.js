9fda80442ba277cdca3d2ef80273d103522eb856
sleman safiah <slemansafiah43@gmail.com>
2021-08-27 13:12:14 +0300

    add swagger next

--- src/app/service/service.js
+++ src/app/service/service.js
@@ -5,15 +5,14 @@ const Service = require('./Services');
 class ServicesService {
 	constructor(data) {
 		this.name = data.name;
-		this.age = data.age;
-		this.public = 10;
-		this.private = 10;
-		this.optional = data.optional;
-		this.type = data.type;
-		this.width = data.width;
-		this.hight = data.hight;
-		this.lang = data.lang;
-		this.long = data.long;
+		this.institution = data.institution;
+		this.category = data.category;
+		this.description = data.description;
+		this.length = data.length;
+		this.price = data.price;
+		this.atLeast = data.atLeast;
+		this.retainer = data.retainer;
+		this.hasRetainer = data.hasRetainer;
 	}
 
 	async save() {
@@ -25,27 +24,21 @@ class ServicesService {
 	}
 
 	async update(id) {
-		const result = await Service.findByIdAndUpdate(id, this);
-		if (!result) throw new Exception(httpStatus.NOT_FOUND, 'Item not found');
+		const result = await Service.findByIdAndUpdate(id, this, { omitUndefined: true });
+		if (!result) throw new Exception(httpStatus.NOT_FOUND, 'Service not found');
 		return;
 	}
 
 	static async delete(id) {
 		const result = await Service.findByIdAndDelete({ _id: id });
-		if (!result) throw new Exception(httpStatus.NOT_FOUND, 'Item not found');
+		if (!result) throw new Exception(httpStatus.NOT_FOUND, 'Service not found');
 		return;
 	}
 
 	static async getById(id) {
 		const result = await Service.findById(id);
-		if (!result) throw new Exception(httpStatus.NOT_FOUND, 'Item not found');
+		if (!result) throw new Exception(httpStatus.NOT_FOUND, 'Service not found');
 		return { data: result };
-	}
-
-	static async getByCriteria(criteria = {}, { limit = 100, skip = 50, total = false }) {
-		const result = { data: await Service.find(criteria, {}, { limit, skip }) };
-		if (total) result.total = Service.countDocuments(criteria);
-		return result;
 	}
 }
 
