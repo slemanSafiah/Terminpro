4cb60a5d5d799089c904aa9875684f0b5ecf1ae6
sleman safiah <slemansafiah43@gmail.com>
2021-09-13 12:05:31 +0300

    fix email bugs

--- utils/helper/email.js
+++ utils/helper/email.js
@@ -1,25 +1,32 @@
-const nodemailer = require('nodemailer');
-const template = require('./template');
+const axios = require('axios');
 
 module.exports = async (token, email) => {
 	const link = `http://lcoalhost:8080/resetpass?token=${token}`;
-	const html = template(link);
 
-	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465, //465
-		secure: true,
-		auth: {
-			user: 'slemansafiah45@gmail.com',
-			pass: '5771125sss',
+	const data = {
+		service_id: process.env.SERVICEID,
+		template_id: process.env.TEMPLATEID,
+		user_id: process.env.USERID,
+		template_params: {
+			email: email,
+			link: link,
 		},
-	});
+		accessToken: process.env.ACCESSTOKEN,
+	};
 
-	const info = await transporter.sendMail({
-		from: 'test@test.com', // sender address
-		to: email, // list of receivers
-		subject: 'Reset Password', // Subject line
-		html: html, // html body
-	});
-	return 'done';
+	await axios({
+		method: 'POST',
+		url: 'https://api.emailjs.com/api/v1.0/email/send',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		data: data,
+	}).then(
+		(response) => {
+			console.log('SUCCESS!', response.status, response.data);
+		},
+		function (error) {
+			console.log('FAILED...', error.response.data);
+		}
+	);
 };
